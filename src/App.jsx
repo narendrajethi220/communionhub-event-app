fc2af2819f9326bacc588a86452b2f1a6208bba8
Narendra Jethi <narendrajethi220@gmail.com>
2025-03-12 17:16:10 +0530

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import { useState } from 'react';
-import {Routes, Route} from 'react-router-dom';
-import  {Header}  from "./components/Header"
-import Home from "./pages/Home"
-import Events from './pages/Events';
-import EventForm from './components/EventForm';
-import initialEvents from './data/eventData';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import EventForm from "./components/EventForm";
+import initialEvents from "./data/EventData.jsx";
 const App = () => {
-  const [events, setEvents]=useState(initialEvents);
-  
-  const addEvent=(newEvent)=>{
-    setEvents([newEvent,...events]);
-  }
+  const [events, setEvents] = useState(initialEvents);
+
+  const addEvent = (newEvent) => {
+    setEvents([newEvent, ...events]);
+  };
   return (
     <div>
-        <Header/>
-        <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/events' element={<Events events={events}/>}/>
-            <Route path='/addevents' element={<EventForm addEvent={addEvent}/>}/>
-        </Routes>
-        
-        </div>
-  )
-}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events events={events} />} />
+        <Route path="/addevents" element={<EventForm addEvent={addEvent} />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
