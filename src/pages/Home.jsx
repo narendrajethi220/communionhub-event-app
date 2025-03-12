88cb5b5d9bef6cde8af994655966825b7d34b974
Narendra Jethi <narendrajethi220@gmail.com>
2025-03-12 19:02:52 +0530

    fixing css for responsive device

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,43 +4,68 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home-container">
-      <header id="hero">
-      <h1 id="hero-main-title">Welcome to <span id="hero-title"> Communion </span> App</h1>
-      <p id="hero-subtitle">Connecting People Across Faiths & Interests</p>
-     </header>
-     </div> 
-     <div className="hero-features">
+        <header id="hero">
+          <h1 id="hero-main-title">
+            Welcome to <span id="hero-title"> Communion </span> App
+          </h1>
+          <p id="hero-subtitle">Connecting People Across Faiths & Interests</p>
+        </header>
+      </div>
+      <div className="hero-features">
         <div className="hero-features-header">
-    
-        <img src="/images/advantages.png"/>
-    
-        <h2 className="hero-feature-title">Unite, Innovation,<span className="feature-img"><img src="/images/feature-image-2.png"/></span></h2>
-        <h2 className="hero-feature-title"><span className="feature-img"><img src="/images/feature-image-1.png"/></span> Connect, Inspires </h2>
-        <h2 className="hero-feature-title">
-        Together <span className="feature-img"><img src="/images/feature-people-image.png"/></span>
-        </h2>
-        
-        <p className="hero-feature-subtitle">
-        Connecting people of all faiths through events and community support.
-        </p>
-       
-       <div className="btn-explore-events">
-       <Link className="explore-event" to="/events">Explore Events <span><img
-       className="icon-arrow" src="/images/icon-arrow.png"/></span></Link>
-       </div>
+          <img src="/images/advantages.png" />
 
+          <h2 className="hero-feature-title">
+            Unite, Innovation,
+            <span className="feature-img1">
+              <img src="/images/feature-image-2.png" />
+            </span>
+          </h2>
+          <h2 className="hero-feature-title">
+            <span className="feature-img2">
+              <img src="/images/feature-image-1.png" />
+            </span>{" "}
+            Connect, Inspires{" "}
+          </h2>
+          <h2 className="hero-feature-title">
+            Together{" "}
+            <span className="feature-img3">
+              <img src="/images/feature-people-image.png" />
+            </span>
+          </h2>
+
+          <p className="hero-feature-subtitle">
+            Connecting people of all faiths through events and community
+            support.
+          </p>
+
+          <div className="btn-explore-events">
+            <Link className="explore-event" to="/events">
+              Explore Events
+              <span>
+                <img className="icon-arrow" src="/images/icon-arrow.png" />
+              </span>
+            </Link>
+          </div>
         </div>
-    
+
         <div className="hero-features-images">
-        <img className="hero-feature-image" src="/images/feature-event-image-2.webp"/>
-        <img className="hero-feature-image" src="/images/feature-event-image-3.avif"/>
-        <img className="hero-feature-image" src="/images/feature-event-image-1.avif"/>
-        
+          <img
+            className="hero-feature-image"
+            src="/images/feature-event-image-2.webp"
+          />
+          <img
+            className="hero-feature-image"
+            src="/images/feature-event-image-3.avif"
+          />
+          <img
+            className="hero-feature-image"
+            src="/images/feature-event-image-1.avif"
+          />
         </div>
-     </div>
-    
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
